eb901a0772e44a2b6c68d41beb468b65b0114955
TzuYuan <tzuyuanliu0830@gmail.com>
2016-07-07 00:10:05 +0800

    fix migration bug

--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,10 @@ ActiveRecord::Schema.define(version: 20160629104144) do
   add_index "post_tag_ships", ["tag_id"], name: "index_post_tag_ships_on_tag_id"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "content"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "title"
+    t.string   "content"
     t.integer  "user_id"
     t.datetime "last_comment_time"
     t.integer  "most_comment"
